8a1dbf5bacc55ee74b95c09ccbc817d380db327b
snhasan5 <snhasan2005>
2025-07-05 15:37:23 +0530

    Use environment variables for database config, added .gitignore

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import pg from "pg"
+import dotenv from "dotenv";
+dotenv.config();
 const db = new pg.Client({ // connecting to database
-  user :"postgres",
-  host : "localhost",
-  database : "Library",
-  password : "hasan",
-  port : 5432
+user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 
 });
 db.connect();
